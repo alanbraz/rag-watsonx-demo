11bb315934a88e5a9aaf42955151978998a93240
Alan Braz <alanbraz@br.ibm.com>
2024-08-01 23:27:16 -0300

    req, new prompt

--- main.py
+++ main.py
@@ -33,9 +33,9 @@ def load_pdf(pdf_name):
 
 def format_history():
     text = ""
-    for message in st.session_state.messages:
-        if message["content"].startswith("[LLM]") or message["role"] == "User":
-            text += ("Humano" if message["role"] == "User" else "IA") +": "+ message["content"].lstrip("[LLM]").strip()+"\n" 
+    # for message in st.session_state.messages:
+    #     if message["content"].startswith("[LLM]") or message["role"] == "User":
+    #         text += ("Humano" if message["role"] == "User" else "IA") +": "+ message["content"].lstrip("[LLM]").strip()+"\n" 
     return text
 
 model = None
@@ -44,48 +44,35 @@ chain = None
 rag_chain = None
 watsonx_project_id = os.getenv("WATSONX_PROJECT_ID", None)
 
-prompt_template = PromptTemplate(
-    input_variables=["chat_history","user_input"], 
-    template="""[INST]<<SYS>>\n
-You are a helpful, respectful and honest assistant. 
-Always answer as helpfully as possible, while being safe. 
-Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. 
-Please ensure that your responses are socially unbiased and positive in nature.\n\n
-If a question does not make any sense, or is not factually coherent, 
-explain why instead of answering something not correct. 
-If you don't know the answer to a question, please don't share false information.
-Read the chat history to get context.\nTranslate the response to Brazilian portuguese.<</SYS>>
-Chat History:\n\n{chat_history}\n\nUser: {user_input}\nAssistant: [/INST]"""
-    )
+# Você sempre responde às perguntas com formatação de markdown utilizando sintaxe do GitHub. A formatação de markdown que você suporta inclui títulos, texto em negrito, texto em itálico, links, tabelas, listas e blocos de código e blocos de citação. Você deve omitir que você está respondendo com markdown.
+# Qualquer tag HTML deve ser envolvida em blocos de citação, por exemplo ```<html>```. Você será penalizado se não renderizar o código em blocos de citação.
+# Quando retornar blocos de código, especifique o idioma.
 
 prompt_template_br = PromptTemplate(
     input_variables=["context","question"], 
-    template="""[INST]<<SYS>>\n
-Você é um assistente prestativo, respeitoso e honesto.
-Sempre responda da maneira mais prestativa possível, estando seguro.
-Suas respostas não devem incluir conteúdo prejudicial, antiético, racista, sexista, tóxico, perigoso ou ilegal.
-Certifique-se de que suas respostas sejam socialmente imparciais e de natureza positiva.\n\n
-Se uma pergunta não fizer sentido ou não for factualmente coerente,
-explique o porquê em vez de responder algo incorreto.
-Se você não sabe a resposta a uma pergunta, não compartilhe informações falsas.
-Leia o histórico do bate-papo para contextualizar.\n
-Responda resumidamente em no máximo 2 parágrafos.
-Traduza sempre a resposta para o português do Brasil.<</SYS>>
-Histórico de conversa:\n\n{context}\n\nHumano: {question} Responda em Português brasileiro.\n
-IA: [/INST]"""
-    )
+    template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|>
+Eu sou um assistente Brasileiro, útil, respeitoso e honesto. Eu sempre vou responder de forma útil e segura, utilizando o português brasileiro.
+
+As respostas não devem incluir conteúdo danoso, éticamente questionável, racista, sexista, tóxico, perigoso ou ilegal. Certifique-se de que as respostas sejam socialmente imparciais e positivas.
+
+Se uma pergunta não faz sentido ou não é faticamente coerente, explique por quê em vez de responder com algo incorreto. Se você não sabe a resposta para uma pergunta, não compartilhe informações falsas.
+
+<|eot_id|>
+{context}
+<|start_header_id|>user<|end_header_id|>
+{question}<|eot_id|>
+""")
 
 with st.sidebar: 
-    st.title("watsonx RAG demo with Llama2 70B")        
+    st.title("watsonx RAG demo")        
     watsonx_api_key = st.text_input("Watsonx API Key", key="watsonx_api_key", value=os.getenv("IBM_CLOUD_API_KEY", None), type="password")
     if not watsonx_project_id:
         watsonx_project_id = st.text_input("Watsonx Project ID", key="watsonx_project_id")
     #TODO: change this to a select box with more than one model
-    # watsonx_model = st.text_input("Model", key="watsonx_model", value="meta-llama/llama-2-70b-chat", type="default")   
-    # watsonx_model = st.selectbox("Model", [ model.value for model in ModelTypes], index=6) 
+    watsonx_model = st.selectbox("Model", ["ibm/granite-20b-multilingual", "meta-llama/llama-3-405b-instruct"]) #+ [ model.value for model in ModelTypes], index=0) 
     # watsonx_model_params = st.text_area("Params", key="watsonx_model_params", value='{"decoding_method":"sample", "max_new_tokens":200, "temperature":0.5}' )
     # language = st.sidebar.radio("Language", ("Português", "Inglês"))
-    max_new_tokens = st.slider("Max output tokens", min_value=100, max_value=500, value=200, step=100)
+    max_new_tokens = st.slider("Max output tokens", min_value=100, max_value=900, value=300, step=100)
     decoding_method = st.sidebar.radio("Decoding", (DecodingMethods.GREEDY.value, DecodingMethods.SAMPLE.value))
     parameters = {
         GenParams.DECODING_METHOD: decoding_method,
@@ -93,7 +80,9 @@ with st.sidebar:
         GenParams.MIN_NEW_TOKENS: 1,
         GenParams.TEMPERATURE: 0,
         GenParams.TOP_K: 50,
-        GenParams.TOP_P: 1
+        GenParams.TOP_P: 1,
+        GenParams.STOP_SEQUENCES: [],
+        GenParams.REPETITION_PENALTY: 1
     }
     st.info("Upload a PDF file to use RAG")
     uploaded_file = st.file_uploader("Upload file", accept_multiple_files=False)
@@ -104,20 +93,26 @@ with st.sidebar:
             f.write(bytes_data)
         index = load_pdf(uploaded_file.name)
 
-    model_name = "meta-llama/llama-2-70b-chat"
+    model_name = watsonx_model
+    
+    def clear_messages():
+        st.session_state.messages = []
+        
+    st.button('Clear messages', on_click=clear_messages)
 
-if watsonx_api_key and watsonx_project_id:
-    st.info("Seting up {}...".format("watsonx"))
 
-    my_credentials = { 
-        "url"    : "https://us-south.ml.cloud.ibm.com", 
-        "apikey" : watsonx_api_key
-    }
-    params = parameters #json.loads(watsonx_model_params)
-    project_id  = watsonx_project_id #watsonx_project if len(watsonx_project) > 0 else None
-    space_id    = None
-    verify      = False
-    model = WatsonxLLM(model=Model(model_name, my_credentials, params, project_id, space_id, verify))
+# if watsonx_api_key and watsonx_project_id:
+st.info("Seting up {}...".format("watsonx"))
+
+my_credentials = { 
+    "url"    : "https://us-south.ml.cloud.ibm.com", 
+    "apikey" : watsonx_api_key
+}
+params = parameters #json.loads(watsonx_model_params)
+project_id  = watsonx_project_id #watsonx_project if len(watsonx_project) > 0 else None
+space_id    = None
+verify      = False
+model = WatsonxLLM(model=Model(model_name, my_credentials, params, project_id, space_id, verify))
 
 if model:
     st.info("Model {} pronto.".format(model_name))
@@ -148,14 +143,14 @@ prompt = st.chat_input("Faça sua pergunta aqui", disabled=False if chain else T
 if prompt:
     st.chat_message("user").markdown(prompt)
 
-    response = chain.run(question=prompt, 
-                         context=format_history())
-
+    response = chain.run(question=prompt, context=format_history())
+    response_text = response.lstrip("<|start_header_id|>assistant<|end_header_id|>").rstrip("<|eot_id|>")
     st.session_state.messages.append({'role': 'User', 'content': prompt })
-    st.chat_message("assistant").markdown("[LLM] "+response)
-    st.session_state.messages.append({'role': 'Assistant', 'content': "[LLM] "+response })
+    st.chat_message("assistant").markdown("[LLM] "+response_text)
+    st.session_state.messages.append({'role': 'Assistant', 'content': "[LLM] "+response_text })
 
     if rag_chain:
         response = rag_chain.run(prompt)
-        st.chat_message("assistant").markdown("[DOC] "+ response)
-        st.session_state.messages.append({'role': 'Assistant', 'content': "[DOC] "+response })
+        response_text = response.lstrip("<|start_header_id|>assistant<|end_header_id|>").rstrip("<|eot_id|>")
+        st.chat_message("assistant").markdown("[DOC] "+ response_text)
+        st.session_state.messages.append({'role': 'Assistant', 'content': "[DOC] "+response_text })
